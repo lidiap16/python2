0ae729375389b6a6f403abef63d981e2d5c29776
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2025-02-06 23:09:54 +0000

    Add files via upload

--- function practice.py
+++ function practice.py
@@ -54,50 +54,88 @@
 #task 1
 #05/02/2025
 
-def name():
-    name = input("enter name:")
-    print("hello", name)
-name()
+# def name():
+#     name = input("enter name:")
+#     print("hello", name)
+# name()
+# 
+# #task2
+# def ave(grade1, grade2):
+#     average = (grade1 + grade2) / 2
+#     return average
+# grade1 = 3
+# grade2 = 5
+# average = ave(grade1, grade2)
+# print(average)
+# 
+# #task3
+# def classave(grades):
+#     classaverage = sum(grades) / 10
+#     return classaverage
+# grades = (12, 90, 56, 78, 58, 98, 67, 87, 98, 87)
+# classaverage = classave(grades)
+# print(classaverage)
+# 
+# #task4
+# def validgrade(grade):
+#     if grade < 100 and grade > 0:
+#         return True
+#     else:
+#         return False
+# grade = 198   
+# if validgrade(grade):
+#     print("valid")
+# else:
+#     print("invalid")
+# 
+# #task5
+#     
+# grades = [67, 67, 78, 98, 87, 78, 78, 98, 90]
+# def gradescal():
+#     grade = int(input("enter grade:"))
+#     if grade < 100 and grade > 0:
+#         grades.append(grade)
+#     else:
+#         print("invalid")
+#         
+# 
+# gradescal()
+# print(grades)
 
-#task2
-def ave(grade1, grade2):
-    average = (grade1 + grade2) / 2
-    return average
-grade1 = 3
-grade2 = 5
-average = ave(grade1, grade2)
-print(average)
+# #task8
+# def lowgrade(grade):
+#     return min(grade)
+# grade = [32, 45, 12, 98]
+# 
+# print(lowgrade(grade))
 
-#task3
-def classave(grades):
-    classaverage = sum(grades) / 10
-    return classaverage
-grades = (12, 90, 56, 78, 58, 98, 67, 87, 98, 87)
-classaverage = classave(grades)
-print(classaverage)
+#task9#
+# grades = [37, 78, 98, 87, 78, 78, 98, 90]
+# def gradescal():
+#     grade = int(input("enter grade:"))
+#     if grade < 100 and grade >= 50:
+#         grades.append(grade)
+#     else:
+#         print("invalid")
+#    # if grades < 50 and grades >= 0:
+#     #    grades.remove(grades)
+#         
+# 
+# gradescal()
+# print(grades)
 
-#task4
-def validgrade(grade):
-    if grade < 100 and grade > 0:
-        return True
-    else:
-        return False
-grade = 198   
-if validgrade(grade):
-    print("valid")
-else:
-    print("invalid")
+#task10
+def count_pass(grades):
+    return sum(1 for grade in grades if grade >= 50)
 
-#task5
-    
-grades = [67, 67, 78, 98, 87, 78, 78, 98, 90]
-def gradescal():
-    grade = int(input("enter grade:"))
-    if grade < 100 and grade > 0:
-        grades.append(grade)
-    else:
-        print("invalid")
-        
-
-gradescal()
-print(grades)
+def graderep(grades):
+    highest = max(grades)
+    lowest = min(grades)
+    ave = sum(grades) / len(grades)
+    passing = count_pass(grades)
+print(highest)
+print(lowest)
+print(ave)
+print(passing)
+grade_list = [85, 92, 47, 76, 50, 89, 33, 100, 67, 45]
+graderap(grades)
